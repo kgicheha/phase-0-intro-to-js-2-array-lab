d44c13ad4cd4bb7bd5837a810d6a8945b09e45e2
Kevin Gicheha <gichehakevin@gmail.com>
2022-06-06 19:14:44 -0700

    commit tree 07237a3943f2cf1b9f1ab68cd06cf3aa7808be28

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"] ;
+
+
+function  destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    let newcatsArray = cats.slice();
+    newcatsArray.push(name);
+    return newcatsArray;
+}
+
+function prependCat(name) {
+    let newcatsArray = cats.slice();
+    newcatsArray.unshift(name);
+    return newcatsArray;
+}
+
+function removeLastCat() {
+    let newcatsArray = cats.slice();
+    newcatsArray.pop();
+    return newcatsArray;
+}
+
+function removeFirstCat() {
+    let newcatsArray = cats.slice();
+    newcatsArray.shift();
+    return newcatsArray;
+}
